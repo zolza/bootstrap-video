da1858b289586b9c4a87c6f486881c485eaea234
zolza <kinga.koziol6@gmail.com>
2015-05-25 22:49:01 +0200

    menu is responsive, fixed js functions and navbar

--- js/scrollmenu.js
+++ js/scrollmenu.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
   $(".navbar a").on('click', function(event) {
-    event.preventDefault(); //preventDefault blokuje domyślne zachowanie przeglądaki, mam tutaj linki
+    event.preventDefault();
     var element = $(event.target).attr('data-target');
 
     $('html, body').animate({
-        scrollTop: $(element).offset().top -200
+        scrollTop: $(element).offset().top -20
     }, 1000);
   });
 });
+
+
+$(document).ready(function(){
+    $(window).on("resize", function(){
+        if ($(window).width() < 1160) {
+            $(".navbar").addClass("navbar2");
+        } else {
+            $(".navbar").removeClass("navbar2");
+        }
+    });
+});
+
