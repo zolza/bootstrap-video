6b8e4ecf51af0c6c41924457bd62c7b2a384312c
zolza <kinga.koziol6@gmail.com>
2015-05-27 21:24:14 +0200

    Working on button to be attached to jquery actions

--- js/scrollmenu.js
+++ js/scrollmenu.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
     $(window).on("resize", function(){
         if ($(window).width() < 1160) {
             $(".navbar").addClass("navbar2");
+            $(".navbar br").remove();
         } else {
             $(".navbar").removeClass("navbar2");
         }
